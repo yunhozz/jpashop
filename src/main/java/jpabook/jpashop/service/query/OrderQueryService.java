46e1af4eae211a22c800b112a7a668ba5b79deb4
yunhozz <qkrdbsgh1121@naver.com>
2022-03-27 18:38:15 +0900

    first commit

--- src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
+++ src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -1,7 +1,18 @@
 package jpabook.jpashop.service.query;
 
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class OrderQueryService {
+
+    /**
+     * 화면이나 API 에 맞춘 서비스 (주로 읽기 전용 트랜잭션 사용)
+     */
+
+    private final OrderRepository orderRepository;
 }
